c412ab389f857704dd2950d8e617d4b86184e197
estka95 <estkamail@gmail.com>
2021-08-23 14:03:30 +0700

    add main function

---
+++ src/main/kotlin/bangun-datar.kt
@@ -0,0 +1,24 @@
+fun hitungLuas(panjang: Int, lebar:Int): Int {
+        return panjang*lebar
+    }
+fun luasSegitiga(alas: Int, tinggi: Int): Double {
+    return (0.5 * alas * tinggi)
+}
+
+fun main(args: Array<String>) {
+    println("Luas persegi: " +hitungLuas(20,10))
+    println("Luas segitiga: " +luasSegitiga(10,4))
+
+
+    print("Panjang persegi: ")
+    var panjang1: Int = readLine()!!.toInt()
+
+    print("lebar persegi: ")
+    var lebar2: Int = readLine()!!.toInt()
+
+    val luasPersegi = panjang1*lebar2
+
+    println("Luas persegi adalah $luasPersegi")
+}
+
+
